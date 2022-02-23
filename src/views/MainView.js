a5399c9b0c928bd0365aa31e34d65746ded6cd90
chrisbelloso <christianbelloso2002@gmail.com>
2022-02-23 00:34:51 -0500

    added projects

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -15,21 +15,20 @@ const MainView = () => {
         <div className='allProjects'>
             <div className='exploreContainer'>
                 <h2 style={{fontWeight: "bold"}}>The Help App</h2>
-                <p>The Help App is a website used to get immediate help after a crash. 
+                <p>- The Help App is a website used to get immediate help after a crash. 
                     It also has a pre-saved "favorites" feature for a faster contact with 
                     your loved ones on a moment of distress.</p>
                 <a className='toLinkContainer' href="https://jolly-shannon-f518c5.netlify.app/">GO</a>
             </div>
             <div className='exploreContainer'>
                 <h2 style={{fontWeight: "bold"}}>Chris Pool</h2>
-                <p>Here is a look at the pool game that I created using Javascript during 
+                <p>- Here is a look at the pool game that I created using Javascript during 
                     project week at IRONHACK!!</p>
-                <a className='toLinkContainer' href="https://jolly-shannon-f518c5.netlify.app/">GO</a>
+                <a className='toLinkContainer' href="https://chrisbelloso.github.io/POOL-GAME/">GO</a>
             </div>
             <div className='exploreContainer'>
-                <h2 style={{fontWeight: "bold"}}>Chris Pool</h2>
-                <p>Here is a look at the pool game that I created using Javascript during 
-                    project week at IRONHACK!!</p>
+                <h2 style={{fontWeight: "bold"}}>Address Book:</h2>
+                <p>- An address book containing 50+ user data from API ending point.</p>
                 <a className='toLinkContainer' href="https://jolly-shannon-f518c5.netlify.app/">GO</a>
             </div>
         </div>
